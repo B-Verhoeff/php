9e301654bd389baade34c3323bbc421390c75a1d
B-Verhoeff <302644473@student.rocmondriaan.nl>
2024-11-18 00:57:03 +0100

    alles weg

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-$title = 'php is awesome';
-require 'index.view.php';
--- opdracht1.php
+++
@@ -1,5 +0,0 @@
-<?php
-$prijs = 50;
-if ($prijs < 55) {
-    echo "oude prijs: prijs: $prijs. Na verhoging 11% is het". ($prijs * 1.11);
-}
